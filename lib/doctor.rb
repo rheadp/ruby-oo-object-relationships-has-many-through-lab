54df3f3e967881ee4f2bc17158c3f36ef457782f
rheadp <rheadpea@msu.edu>
2021-03-02 16:32:18 -0500

    commit tree 0d56ba88ca5ee6312f9dd22d2e89a742e9ab5f30

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,3 @@
-
+class Doctor
+end
+    
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,20 @@
+class Genre
+    attr_accessor :song, :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def songs 
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        Song.all.map {|song| song.artist}
+    end
+end
